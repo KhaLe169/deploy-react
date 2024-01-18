74197b2b787792933927af7f65f63d44aed1f8f8
KhaLe169 <lekha1998zz@gmail.com>
2024-01-18 23:36:03 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Router from "./Components/Router";
+import Router from './Components/Router'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-//tést
+
 function App() {
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./css/style.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './App.css'
+import './css/style.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
